ae3bd0d3921875a60eb2f70aa2537951ff5650cc
ElegantCheetah <Zain4utah@gmail.com>
2021-04-28 13:48:38 -0600

    Update DoritoRobot.ino

--- DoritoRobot/DoritoRobot.ino
+++ DoritoRobot/DoritoRobot.ino
@@ -1,15 +1,16 @@
+//Set the motor pin numbers here:
 #define rightMotor 3
 #define leftMotor 5
 /*
 TODO: write code for the movement of robot
-
+TODO: Learn how to communicate with REV spark MAX
 Neo robot brushless motors:
 Right motor is pin 3
 Left motor is pin 5
 */
 void setup() {
-  pinMode(rightMotor, OUTPUT);
-  pinMode(leftMotor, OUTPUT);
+  pinMode(rightMotor, OUTPUT); //Right motor is setup to be an output
+  pinMode(leftMotor, OUTPUT); //Left motor is setup to be an output
 }
 
 void loop() {
