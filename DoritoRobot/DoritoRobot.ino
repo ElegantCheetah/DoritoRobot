8a064b2b358705cee5d841fd3e9fd92ef0f31ebe
ElegantCheetah <Zain4utah@gmail.com>
2021-04-28 17:38:37 -0600

    Fully functional movement code for robot

--- DoritoRobot/DoritoRobot.ino
+++ DoritoRobot/DoritoRobot.ino
@@ -1,4 +1,4 @@
-#include <servo.h>
+#include <Servo.h>
 
 //Set the pin numbers here:
 #define rightMotor 3
@@ -28,9 +28,25 @@ void setup() {
   pinMode(potInputR, INPUT); //Potentiometers are set as inputs
   pinMode(potInputL, INPUT);
   pinMode(potInputB, INPUT);
+
+  //Servo Initialization here:
+  Servo rightMotor;
+  Servo leftMotor;
+
+  //Attaching servo's to the right pin
+  rightMotor.attach(rightMotor);
+  leftMotor.attach(leftMotor);
 }
 
 void loop() {
-  //Stuff here
+  //Mapping the values from potentiometer from 0-1023 to 0-180
+  int potR;
+  int potL;
+  potR = map(analogRead(potInputR),0,1023,0,180);
+  potL = map(analogRead(potInputL),0,1023,0,180);
+
+  //Sending pwm signal to both motors
+  rightMotor.write(potR);
+  leftMotor.write(potL);
 
 }
