174c19407727f2b0b6cdaac19a21b146803186e1
ElegantCheetah <Zain4utah@gmail.com>
2021-04-28 14:07:43 -0600

    Update DoritoRobot.ino

--- DoritoRobot/DoritoRobot.ino
+++ DoritoRobot/DoritoRobot.ino
@@ -1,19 +1,36 @@
-//Set the motor pin numbers here:
+#include <servo.h>
+
+//Set the pin numbers here:
 #define rightMotor 3
 #define leftMotor 5
+#define potInputR 7
+#define potInputL 8
+#define potInputB 9
 /*
-TODO: write code for the movement of robot
+TODO: Write code for the movement of robot
 TODO: Learn how to communicate with REV spark MAX
-Neo robot brushless motors:
-Right motor is pin 3
-Left motor is pin 5
+TODO: Take potentiometer input and make it pwm signal
+ 
+
+
+Refresher:
+
+Motor contollers expect pulses every 20 ms and a pulse width of 1.5
+Pulse width of 1.0 is full reverse
+Pulse width of 2.0 is full forward
+
+Use servo commands 
+Map 0-1023 to 0-180 for motor controllers
 */
 void setup() {
   pinMode(rightMotor, OUTPUT); //Right motor is setup to be an output
   pinMode(leftMotor, OUTPUT); //Left motor is setup to be an output
+  pinMode(potInputR, INPUT); //Potentiometers are set as inputs
+  pinMode(potInputL, INPUT);
+  pinMode(potInputB, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Stuff here
 
 }
