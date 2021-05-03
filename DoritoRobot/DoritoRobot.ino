225e81601fcbe120b63a5371627d4beaa7d81df0
ElegantCheetah <Zain4utah@gmail.com>
2021-05-03 10:47:21 -0600

    Update DoritoRobot.ino

--- DoritoRobot/DoritoRobot.ino
+++ DoritoRobot/DoritoRobot.ino
@@ -5,27 +5,12 @@
 #define leftMotor 5
 #define potInputR 7
 #define potInputL 8
-#define potInputB 9
+#define potInputB 9 //Not needed for now
 
 //Servo Initialization here:
 Servo rightMotorCont;
 Servo leftMotorCont;
 
-/*
-TODO: Learn how to communicate with REV spark MAX
-TODO: Take potentiometer input and make it pwm signal
- 
-
-
-Refresher:
-
-Motor contollers expect pulses every 20 ms and a pulse width of 1.5
-Pulse width of 1.0 is full reverse
-Pulse width of 2.0 is full forward
-
-Use servo commands 
-Map 0-1023 to 0-180 for motor controllers
-*/
 void setup() {
   //Setting the pins to be inputs and outputs
   pinMode(rightMotor, OUTPUT); 
