bf738b745c4e332736f2cffc6920cac87981062a
ElegantCheetah <Zain4utah@gmail.com>
2021-05-17 14:31:41 -0600

    Changed variable location

--- DoritoRobot/DoritoRobot.ino
+++ DoritoRobot/DoritoRobot.ino
@@ -13,8 +13,14 @@ Servo rightMotorCont;
 Servo leftMotorCont;
 Servo armMotorCont;
 
+//Variable initialization here
+int potR;
+int potL;
+int potA;
+
 void setup() {
   //Setting the pins to be inputs and outputs
+  pinMode(armMotor, OUTPUT);
   pinMode(rightMotor, OUTPUT); 
   pinMode(leftMotor, OUTPUT); 
   pinMode(potInputR, INPUT); 
@@ -25,13 +31,8 @@ void setup() {
   rightMotorCont.attach(rightMotor);
   leftMotorCont.attach(leftMotor);
   armMotorCont.attach(armMotor);
-
-  //Variable initialization here
-  int potR;
-  int potL;
-  int potA;
 }
-
+  
 void loop() {
   //Mapping the values from potentiometer from 0-1023 to 0-180
   potR = map(analogRead(potInputR),0,1023,0,180);
